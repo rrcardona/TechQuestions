41ada06e108551cfe11e03eb018337199f53b73c
rrcardona <cardonas05@yahoo.com>
2017-08-22 18:14:18 -0400

    First Commit

---
+++ Tech Interview Questions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_Interview_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number %3==0 && number%5==0)           
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            else if (number%3==0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number%5==0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine("This number is not divisible by 3 or 5");
+            }
+
+
+        }
+    }
+}
